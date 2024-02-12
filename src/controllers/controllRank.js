c5f66a08370b5564392c8631349aa3257f4d1ebc
Matheus Henrique <mathttps@gmail.com>
2024-02-12 05:42:53 -0300

    rank test

--- src/controllers/controllRank.js
+++ src/controllers/controllRank.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.js";
 export async function getRank(req, res) {
     try {
         const query = `
-            SELECT u.id, u.name, COUNT(su.id) AS linksCount, COALESCE(SUM(su."visitorsCount"), 0) AS visitCount
+            SELECT u.id, u.name, COALESCE(COUNT(su.id), 0) AS linksCount, COALESCE(SUM(su."visitorsCount"), 0) AS visitCount
             FROM users u
             LEFT JOIN "shortedUrls" su ON u.id = su."userId"
             GROUP BY u.id, u.name
@@ -13,7 +13,7 @@ export async function getRank(req, res) {
         
         const response = await db.query(query);
         
-        //Teste
+
         const formattedResponse = response.rows.map(user => ({
             id: user.id,
             name: user.name,
@@ -21,12 +21,23 @@ export async function getRank(req, res) {
             visitCount: user.visitCount
         }));
         
+        const missingUsersCount = 10 - formattedResponse.length;
+        for (let i = 0; i < missingUsersCount; i++) {
+            formattedResponse.push({
+                id: null,
+                name: null,
+                linksCount: 0,
+                visitCount: 0
+            });
+        }
+        
         return res.status(200).json(formattedResponse);
     } catch (err) {
         console.error("Erro ao obter o ranking de usuários:", err);
         return res.status(500).send("Erro ao obter o ranking de usuários.");
     }
 }
+
 
 
 
